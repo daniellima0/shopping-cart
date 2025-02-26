8479b3a1b8d423e667a66d7ee1b49451b535960d
Daniel Lima <danielmlimag@gmail.com>
2025-02-26 18:40:18 +0100

    feat: create basic components code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/Home/Home";
+import Shop from "./pages/Shop/Shop";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>hello world</div>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Shop/Shop.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/Navbar/Navbar";
+
+const Shop = () => {
+  return (
+    <>
+      <Navbar />
+      <div>Shop</div>
+    </>
+  );
+};
+
+export default Shop;
